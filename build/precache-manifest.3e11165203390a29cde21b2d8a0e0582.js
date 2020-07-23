a596e0a7e87bdc03841163abb7d8ccefffef1b06
Thibaut Remy <tremy@mavenoid.com>
2020-07-23 14:10:26 +0200

    chore: build

---
+++ build/precache-manifest.3e11165203390a29cde21b2d8a0e0582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27410859a226fb1ecae563aa070f3688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aff92b81272aaf3e978a",
+    "url": "/static/css/main.f3c720d6.chunk.css"
+  },
+  {
+    "revision": "382cb02a77e3955beaa7",
+    "url": "/static/js/2.a894ae92.chunk.js"
+  },
+  {
+    "revision": "eae5bdaf10b6b52fb3852d150f6ba5b9",
+    "url": "/static/js/2.a894ae92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aff92b81272aaf3e978a",
+    "url": "/static/js/main.9d54126f.chunk.js"
+  },
+  {
+    "revision": "c08ad12f92a8ba88dfee",
+    "url": "/static/js/runtime-main.1a3a54c5.js"
+  }
+]);
